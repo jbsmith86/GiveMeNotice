f935f136577526314cc3d79d32d15fb35281f748
Joel Smith <joely5525@gmail.com>
2013-12-29 13:47:37 -0800

    added phone number validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   has_one :twitter_account
   has_many :alerts
   validates :email, presence: true, uniqueness: true
+  validates :sms_number, numericality: true, length: { minimum: 10, maximum: 10 }
   has_secure_password
 end
