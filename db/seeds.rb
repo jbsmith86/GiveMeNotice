e3db07833dca56595491891fdf8fcba58355d06e
Joel Smith <joely5525@gmail.com>
2014-01-05 13:45:03 -0800

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Ted = User.create(email: 'ted@example.com', sms_number: '5551231234', password: '1234', password_confirmation: '1234')
+Ted.alerts = Alert.create(name: 'snoop' , email_enabled: true, sms_enabled: false, feed_username: 'snoopdoggnotreal')
+Ted.alerts.first.keywords.create(phrase: 'tickets')
+
+Linda = User.create(email: 'linda@example.com', sms_number: '2065551234', password: 'password', password_confirmation: 'password')
+Linda.alerts = Alert.create(name: 'comicon' , email_enabled: true, sms_enabled: true, feed_username: 'comiconfake')
+Linda.alerts.first.keywords.create(phrase: 'on sale')
+Linda.alerts.first.keywords.create(phrase: 'coming soon')
