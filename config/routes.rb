0cb401bd8419d3fdaf150309cb9df5c01fd1e8bf
Joel Smith <joely5525@gmail.com>
2014-01-05 15:17:28 -0800

    added static footer pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ GiveMeNotice::Application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :alerts
   get '/auth/twitter/callback', to: 'sessions#create'
+  get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
   root "users#index"
 end
